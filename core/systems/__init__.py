fa06c198292f54b02719236752c5b122bf2d85b0
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2020-11-11 08:42:22 -0800

    Added polynomial system

--- core/systems/__init__.py
+++ core/systems/__init__.py
@@ -2,3 +2,4 @@ from .cart_pole import CartPole
 from .double_inverted_pendulum import DoubleInvertedPendulum
 from .inverted_pendulum import InvertedPendulum
 from .planar_quadrotor import PlanarQuadrotor
+from .polynomial_system import PolynomialSystem
