2f7eccad76232279d29d7f8053c3ccfdf87bb8f0
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2022-02-11 10:32:59 -0800

    Exposed step in batched SystemDynamics class

--- core/systems/batched/system_dynamics.py
+++ core/systems/batched/system_dynamics.py
@@ -9,13 +9,16 @@ class SystemDynamics(ABC, Module):
     def forward(self, xs, us):
         raise NotImplementedError
 
+    def step(self, x_0s, u_0s, t_0, t_f, atol=1e-6, rtol=1e-6):
+        return odeint(lambda t, y: self(y, u_0s), x_0s, tensor([t_0, t_f]), atol=atol, rtol=rtol)[-1]
+
     def simulate(self, x_0s, controller, ts, atol=1e-6, rtol=1e-6):
         state_trajectories = [x_0s]
         action_trajectories = []
         for t_0, t_f in zip(ts[:-1], ts[1:]):
             x_0s = state_trajectories[-1]
             u_0s = controller(x_0s)
-            x_fs = odeint(lambda t, y: self(y, u_0s), x_0s, tensor([t_0, t_f]), atol=atol, rtol=rtol)[-1]
+            x_fs = self.step(x_0s, u_0s, t_0, t_f, atol, rtol)
             state_trajectories.append(x_fs)
             action_trajectories.append(u_0s)
 
