7fa8c49db8067a9f31ea2435c0e1ff01db876d83
ivan <ivan.jimenez@caltech.edu>
2023-05-13 17:06:31 -0700

    [REFACTOR] System observatinal dynamics

--- core/dynamics/observable_dynamics.py
+++ core/dynamics/observable_dynamics.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from torch.autograd.functional import jacobian
-
+from system_dynamics import SystemDynamics
 class ObservableDynamics(ABC):
     def __init__(self, obs_dim):
         self.obs_dim = obs_dim
@@ -17,7 +17,7 @@ class ObservableDynamics(ABC):
     def p(self):
         return self.obs_dim
 
-    def obervable_dynamics(self, observations, actions, t):
+    def make_obervable_dynamics(self):
         """ Returns the dynamics in the observable coordinates.
 
         :param observations: state in the original coordinates (B x n)
@@ -25,10 +25,19 @@ class ObservableDynamics(ABC):
         :param t: time
         :return: dynamics in the observable coordinates (B x obs_dim)
         """
-        state = self.to_principal_coordinates(observations)
-        return self(state, actions, t)
 
-    def observable_dynamics_jacobian(self, observations, actions, t):
+        class ObservableSystem(SystemDynamics):
+            def __init__(inner_self):
+                SystemDynamics.__init__(inner_self, n=self.p, m=self.m)
+
+            def forward(inner_self, y, u, t):
+                x = self.to_principal_coordinates(y)
+                dydx = self.jacobian(x)
+                return dydx @ self(x, u, t)
+
+        return ObservableSystem()
+
+    def jacobian(self, observations):
         """ Returns the jacobian of the dynamics in the observable coordinates.
 
         :param observations: state in the original coordinates (B x n)
@@ -36,9 +45,8 @@ class ObservableDynamics(ABC):
         :param t: time
         :return: jacobian of the dynamics in the observable coordinates (B x obs_dim x n)
         """
-        F, G = jacobian(lambda y, u: self.observable_dynamics(y, u, t).sum(dim=0),
-                 (observations, actions),
+        F = jacobian(lambda x: self.get_observation(x).sum(dim=0),
+                 (observations),
                  create_graph=True)
         F = F.swapaxes(1,0)
-        G = G.swapaxes(1,0)
-        return F, G
+        return F
